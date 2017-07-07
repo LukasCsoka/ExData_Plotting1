77fb563d66d3b156cf81fd1c52da05be800038e3
Lukas Csoka <csoka.lukas@gmail.com>
2017-07-07 11:14:51 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,5 +1,5 @@
-setwd("//CHRB1057.CORP.GWPNET.COM/homes/Z/S6KHZS/Documents/coursera/exploratory")
-rm(list=ls()) ## Vymazanie vsetkych premennych z pamatigc() ## chceck pamate
+rm(list=ls()) ## Vymazanie vsetkych premennych z pamati
+gc() ## chceck pamate
 
 data <- read.table("./household_power_consumption.txt" , header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,] 
